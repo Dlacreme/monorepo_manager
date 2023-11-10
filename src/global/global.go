b857d6cbd43d7f3a1636ef5584e16ca39aa62285
Dlacreme <mathieu.delacroix.fr@gmail.com>
2023-11-10 17:08:10 +0100

    add global

---
+++ src/global/global.go
@@ -0,0 +1,10 @@
+package global
+
+const (
+	// global config
+	ConfigFilePath string = "./monorepo.toml"
+	// Content
+	InvalidCommandLine string = "Couldn't find a valid command to run. Run 'help' for help."
+	CommandNotFound    string = "Command is not found."
+	ExpectArgument     string = "mm expects at least one argument."
+)
