85126eea7ae295af8aa83c709c2aa14a3b4aa3b9
Dlacreme <mathieu.delacroix.fr@gmail.com>
2023-11-10 18:32:29 +0100

    add test for command

---
+++ src/command/command_test.go
@@ -0,0 +1,20 @@
+package command
+
+import (
+	"monorepo_manager/src/global"
+	"monorepo_manager/src/test"
+	"testing"
+)
+
+func TestRun(t *testing.T) {
+	t.Run("can execute 'init'", func(t *testing.T) {
+		err := Run("init", []string{})
+		test.AssertNoError(t, err)
+	})
+
+	t.Run("returns an error if the command is unknown", func(t *testing.T) {
+		err := Run("unknowncommand", []string{})
+		test.AssertErrorEq(t, err, global.CommandNotFound)
+	})
+
+}
